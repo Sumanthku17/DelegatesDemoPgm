e3f58b1ab0107711f83f17c0760ad1ba9affd8e1
SumanthKumar <sumanthkumar.shettyc@mediakind.com>
2024-02-06 19:05:41 +0530

    Added demo PGM

--- DelegatesDemo/Program.cs
+++ DelegatesDemo/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello World!");
+﻿printMsg printConsole = (String msg) =>
+{
+    Console.WriteLine(msg);
+};
+
+printMsg printFile = (String msg) =>
+{
+    File.AppendAllText("./myPath", msg);
+};
+
+printConsole("My msg for you, \"STAY FOCUSED\"");
+
+delegate void printMsg(String msg);
